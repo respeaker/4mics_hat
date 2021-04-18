cd70fde4399fb481ce0c2060e94bca2ac49576cc
lavirott <stephane@lavirotte.com>
2021-04-18 10:50:26 +0200

    Add graceful exist on receiving SIGTERM of SIGINT

--- pixels_demo.py
+++ pixels_demo.py
@@ -1,12 +1,27 @@
+import signal
+import sys
 import time
 from pixels import Pixels, pixels
 from alexa_led_pattern import AlexaLedPattern
 from google_home_led_pattern import GoogleHomeLedPattern
 from rainbow_led_pattern import RainbowLedPattern
 
-if __name__ == '__main__':
+global pixels
 
+class GracefulKiller:
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def exit_gracefully(self,signum, frame):
+        global pixels
+        pixels.off()
+        sys.exit(0)
+
+if __name__ == '__main__':
+    global pixels
     pixels.pattern = RainbowLedPattern(show=pixels.show)
+    killer = GracefulKiller()
 
     while True:
         try:
